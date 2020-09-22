b283dee4e4e62ac66dc9264596ac7e94f3363f7b
Felixatika <felixatika@gmail.com>
2020-09-22 08:20:14 +0300

    add initial project files

---
+++ frontend/views/site/index.php
@@ -0,0 +1,72 @@
+<?php
+
+/* @var $this yii\web\View */
+
+$this->title = 'Felix Atika portfolio';
+?>
+<div class="site-index">
+
+    <div class="jumbotron">
+        <h1>Felix Atika</h1>
+
+        <p class="lead">Full stack developer in Kenya,Nairobi Area</p>
+        <div class="container soc-media">
+        <p><a href="https://github.com/Felixatika"><span class="fa fa-github"></span>GitHub</a></p>
+        <p><a href="https://www.linkedin.com/in/felix-atika-47309a124/"><span class="fa fa-linkedin-square"></span>LinkedIn</a></p>
+        <p><a href="https://twitter.com/felix_atika"><span class="fa fa-twitter"></span>Twitter</a></p>
+        <p><a href="mailto:felixatika@gmail.com"><span class="fa fa-envelope"></span>Email</a></p>
+        </div>
+    </div>
+
+    <div class="body-content">
+        <div class="panel panel-default">
+        <div id="filter-list" class="panel-body">
+            <span id="filter-reset" class="glyphicon glyphicon-repeat"></span>
+        </div>
+        </div>
+        <div class="row project-list">
+            <div class="col-xs-12 col-md-4">
+            <div class="thumbnail">
+            <img src="<?= Yii::$app->request->baseUrl ?>/images/felo.jpeg" alt="test pic">
+            </div>
+            <div class="caption">
+            <span class="label label-primary">Javasript</span>
+            <span class="label label-primary">HTML & CSS</span>
+            <span class="label label-primary">Python</span>
+            <span class="label label-primary">Angular Js</span>
+            <span class="label label-primary">Yii Framework</span>
+            <h3>Felix codes</h3>
+        
+                <h2>Akan Names</h2>
+
+                <p>This project helps user to know their birthdate, by entering their name and date of birth.The application gives the day they were born.Akan names are cultural
+                name in ghana corresponding to the day you were born.The project has been done by Javascriot,css,Html</p>
+                <p>
+                    <a class="btn btn-primary" href="https://felixatika.github.io/Akan-Names/">View &raquo;</a>
+                    <a class="btn btn-default" href="https://github.com/Felixatika/Akan-Names">Source &raquo;</a>
+                  </p>
+                </div>
+            </div>
+            <div class="col-lg-4">
+            <h2>Delani studio</h2>
+
+<p>This is a studio project,where clients can view studio services and contact team.</p>
+<p>
+    <a class="btn btn-primary" href="https://felixatika.github.io/IP3/">View &raquo;</a>
+    <a class="btn btn-default" href="https://github.com/Felixatika/Delani-studio">Source &raquo;</a>
+  </p>
+                
+            </div>
+            <div class="col-lg-4">
+            <h2>Pizza Ordering App</h2>
+
+<p>This is project allows a user to order pizza,checkout or choose options for home delivery then proceed to give the user total cost of ordered items..</p>
+<p>
+    <a class="btn btn-primary" href="https://felixatika.github.io/flixy-pizza-place/">View &raquo;</a>
+    <a class="btn btn-default" href="https://github.com/Felixatika/flixy-pizza-place">Source &raquo;</a>
+  </p>
+            </div>
+        </div>
+
+    </div>
+</div>
